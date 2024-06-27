dc9199fcab3e968582708bb0f2667cfc84e5d42b
Szabó Attila <szabo.attila.cegled@gmail.com>
2024-06-27 10:30:48 +0200

    Updated primary_page theme

--- lib/src/primary_page.dart
+++ lib/src/primary_page.dart
@@ -38,19 +38,15 @@ class _PrimaryPageState extends State<PrimaryPage> {
                   chevron(active: value > 0),
                   InkWell(
                     onTap: () async {
-                      var newDate = await ranger.navKey.currentState!
-                          .pushNamed(SecondaryPage.route, arguments: tabDate);
+                      var newDate =
+                          await ranger.navKey.currentState!.pushNamed(SecondaryPage.route, arguments: tabDate);
                       print(newDate);
-                      if (newDate != null)
-                        widget.onNewDate.call(newDate as DateTime);
+                      if (newDate != null) widget.onNewDate.call(newDate as DateTime);
                     },
                     child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10)
-                          .copyWith(top: 24),
+                      padding: const EdgeInsets.symmetric(horizontal: 10).copyWith(top: 24),
                       child: Text(
-                        DateFormat(
-                                "${DateFormat.ABBR_MONTH} ${DateFormat.YEAR}")
-                            .format(tabDate),
+                        DateFormat("${DateFormat.ABBR_MONTH} ${DateFormat.YEAR}").format(tabDate),
                         style: TextStyle(fontSize: 16.0),
                       ),
                     ),
@@ -64,8 +60,7 @@ class _PrimaryPageState extends State<PrimaryPage> {
           Expanded(
             child: TabBarView(
               controller: ranger.tabController,
-              children: List.generate(
-                  12, (tabIndex) => tabView(tabIndex, constraints.maxWidth)),
+              children: List.generate(12, (tabIndex) => tabView(tabIndex, constraints.maxWidth)),
             ),
           ),
         ],
@@ -79,10 +74,7 @@ class _PrimaryPageState extends State<PrimaryPage> {
         opacity: active ? 1 : 0.2,
         duration: Duration(milliseconds: 100),
         child: InkWell(
-          onTap: active
-              ? () => ranger.tabController
-                  .animateTo(ranger.tabController.index + (left ? -1 : 1))
-              : null,
+          onTap: active ? () => ranger.tabController.animateTo(ranger.tabController.index + (left ? -1 : 1)) : null,
           child: Padding(
             padding: const EdgeInsets.only(top: 24),
             child: Align(
@@ -107,8 +99,7 @@ class _PrimaryPageState extends State<PrimaryPage> {
     return LayoutBuilder(
       builder: (context, constraints) {
         final availableWidth = constraints.maxWidth;
-        var numItemsPerRow =
-            (availableWidth - (availableWidth % itemWidth)) / itemWidth;
+        var numItemsPerRow = (availableWidth - (availableWidth % itemWidth)) / itemWidth;
         return ValueListenableBuilder<DateTimeRange>(
           valueListenable: ranger.dateRange,
           builder: (context, value, child) => Align(
@@ -125,15 +116,10 @@ class _PrimaryPageState extends State<PrimaryPage> {
                     var dateTime = DateTime(year, month, day);
                     var isStart = dateTime.compareTo(value.start) == 0;
                     var isEnd = dateTime.compareTo(value.end) == 0;
-                    var primary = dateTime.compareTo(
-                            ranger.selectingStart ? value.start : value.end) ==
-                        0;
-                    var secondary = dateTime.compareTo(
-                            !ranger.selectingStart ? value.start : value.end) ==
-                        0;
-                    var inRange = dateTime.isBefore(value.end) &&
-                            dateTime.isAfter(value.start) ||
-                        (secondary || primary);
+                    var primary = dateTime.compareTo(ranger.selectingStart ? value.start : value.end) == 0;
+                    var secondary = dateTime.compareTo(!ranger.selectingStart ? value.start : value.end) == 0;
+                    var inRange =
+                        dateTime.isBefore(value.end) && dateTime.isAfter(value.start) || (secondary || primary);
                     var borderRadius = Radius.circular(itemWidth / 2);
 
                     ///Positioned at extreme ends
@@ -143,18 +129,14 @@ class _PrimaryPageState extends State<PrimaryPage> {
                     var isItemsEnd = wrapIndex == daysInMonth - 1;
                     return Transform.translate(
                       offset: Offset(
-                          isStart ||
-                                  isEnd ||
-                                  isExtreme ||
-                                  isItemsEnd && !(isStart && isEnd)
+                          isStart || isEnd || isExtreme || isItemsEnd && !(isStart && isEnd)
                               ? (isStart || isExtremeStart ? 4 : -4)
                               : 0,
                           0),
                       child: Column(
                         mainAxisSize: MainAxisSize.min,
                         children: [
-                          if (ranger.showWeekday &&
-                              (wrapIndex + 1) <= numItemsPerRow) ...[
+                          if (ranger.showWeekday && (wrapIndex + 1) <= numItemsPerRow) ...[
                             SizedBox(height: itemHeight + ranger.runSpacing)
                           ],
                           Container(
@@ -167,16 +149,11 @@ class _PrimaryPageState extends State<PrimaryPage> {
                               height: itemHeight,
                               decoration: BoxDecoration(
                                   borderRadius: BorderRadius.horizontal(
-                                      left: isStart || isExtremeStart
-                                          ? borderRadius
-                                          : Radius.zero,
-                                      right: isEnd || isExtremeEnd || isItemsEnd
-                                          ? borderRadius
-                                          : Radius.zero),
-                                  color:
-                                      inRange && isRange && !(isStart && isEnd)
-                                          ? colorScheme.secondary
-                                          : Colors.transparent)),
+                                      left: isStart || isExtremeStart ? borderRadius : Radius.zero,
+                                      right: isEnd || isExtremeEnd || isItemsEnd ? borderRadius : Radius.zero),
+                                  color: inRange && isRange && !(isStart && isEnd)
+                                      ? colorScheme.secondary
+                                      : Colors.transparent)),
                         ],
                       ),
                     );
@@ -189,45 +166,35 @@ class _PrimaryPageState extends State<PrimaryPage> {
                     var month = tabIndex + 1;
                     var day = wrapIndex + 1;
                     var dateTime = DateTime(year, month, day);
-                    var primary = dateTime.compareTo(
-                            ranger.selectingStart ? value.start : value.end) ==
-                        0;
-                    var secondary = dateTime.compareTo(
-                            !ranger.selectingStart ? value.start : value.end) ==
-                        0;
-                    var inRange = dateTime.isBefore(value.end) &&
-                            dateTime.isAfter(value.start) ||
-                        (secondary || primary);
+                    var primary = dateTime.compareTo(ranger.selectingStart ? value.start : value.end) == 0;
+                    var secondary = dateTime.compareTo(!ranger.selectingStart ? value.start : value.end) == 0;
+                    var inRange =
+                        dateTime.isBefore(value.end) && dateTime.isAfter(value.start) || (secondary || primary);
                     var inRangeTextColor = colorScheme.onPrimary;
                     var outOfRangeTextColor = colorScheme.onBackground;
                     return Column(
                       mainAxisSize: MainAxisSize.min,
                       crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
-                        if (ranger.showWeekday &&
-                            (wrapIndex + 1) <= numItemsPerRow) ...[
+                        if (ranger.showWeekday && (wrapIndex + 1) <= numItemsPerRow) ...[
                           Container(
                             width: itemWidth,
                             height: itemHeight,
                             alignment: Alignment.center,
                             child: Text(
-                              DateFormat(DateFormat.ABBR_WEEKDAY)
-                                  .format(dateTime),
-                              style: TextStyle(
-                                  fontSize: theme.textTheme.bodyText1?.fontSize,
-                                  color: outOfRangeTextColor),
+                              DateFormat(DateFormat.ABBR_WEEKDAY).format(dateTime),
+                              style:
+                                  TextStyle(fontSize: theme.textTheme.bodyMedium?.fontSize, color: outOfRangeTextColor),
                             ),
                           ),
                           SizedBox(height: ranger.runSpacing),
                         ],
                         InkWell(
                           onTap: () async {
-                            var startIsAfterEnd = ranger.selectingStart &&
-                                !dateTime.compareTo(value.end).isNegative &&
-                                isRange;
-                            var endISBeforeStart = !ranger.selectingStart &&
-                                dateTime.compareTo(value.start).isNegative &&
-                                isRange;
+                            var startIsAfterEnd =
+                                ranger.selectingStart && !dateTime.compareTo(value.end).isNegative && isRange;
+                            var endISBeforeStart =
+                                !ranger.selectingStart && dateTime.compareTo(value.start).isNegative && isRange;
                             if (startIsAfterEnd || endISBeforeStart) {
                               widget.onError(startIsAfterEnd
                                   ? "Start date cannot be after end date"
@@ -236,14 +203,9 @@ class _PrimaryPageState extends State<PrimaryPage> {
                               ///set the start and end to the same day if is single picker
                               var newRange = isRange
                                   ? value.copyWith(
-                                      start: ranger.selectingStart
-                                          ? dateTime
-                                          : value.start,
-                                      end: !ranger.selectingStart
-                                          ? dateTime
-                                          : value.end)
-                                  : DateTimeRange(
-                                      start: dateTime, end: dateTime);
+                                      start: ranger.selectingStart ? dateTime : value.start,
+                                      end: !ranger.selectingStart ? dateTime : value.end)
+                                  : DateTimeRange(start: dateTime, end: dateTime);
                               ranger.dateRange.value = newRange;
                               widget.onRangeChanged(newRange);
                             }
@@ -255,22 +217,16 @@ class _PrimaryPageState extends State<PrimaryPage> {
                               alignment: Alignment.center,
                               decoration: BoxDecoration(
                                   shape: BoxShape.circle,
-                                  border: Border.all(
-                                      color: primary
-                                          ? colorScheme.outline
-                                          : Colors.transparent,
-                                      width: 2),
-                                  color: primary || secondary
-                                      ? colorScheme.primary
-                                      : Colors.transparent),
+                                  border:
+                                      Border.all(color: primary ? colorScheme.outline : Colors.transparent, width: 2),
+                                  color: primary || secondary ? colorScheme.primary : Colors.transparent),
                               duration: Duration(milliseconds: 500),
                               child: Text(
                                 "${wrapIndex + 1}",
                                 maxLines: 1,
                                 textAlign: TextAlign.center,
                                 style: TextStyle(
-                                    fontSize:
-                                        theme.textTheme.bodyText1?.fontSize,
+                                    fontSize: theme.textTheme.bodyMedium?.fontSize,
                                     color: primary || secondary
                                         ? inRangeTextColor
                                         : inRange && isRange
--- sample_proj/android/app/src/main/kotlin/com/example/sample_proj/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.sample_proj
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
